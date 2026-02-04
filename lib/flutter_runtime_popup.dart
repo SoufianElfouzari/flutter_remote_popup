2fdcff88998e6181e6e5ebd903b4cd9e6c67bd6d
Soufian El-Fouzari I Promundius <soufian.el-fouzari@promundius.com>
2026-02-04 12:16:14 +0100

    Expose public package API

--- lib/flutter_runtime_popup.dart
+++ lib/flutter_runtime_popup.dart
@@ -1,5 +1,7 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library flutter_runtime_popup;
+
+export "src/runtime_popup_api_clients.dart";
+export "src/runtime_popup_controller.dart";
+export "src/runtime_popup_models.dart";
+export "src/runtime_popup_widget.dart";
+
